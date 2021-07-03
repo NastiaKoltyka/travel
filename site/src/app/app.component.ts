5f0d606a8a794e192d7715317221326509074e41
NastiaKoltyka <Nusia.k5669@gmail.com>
2021-07-03 22:18:36 +0300

    replce date picker

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgbDate, NgbCalendar, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,49 +8,20 @@ import { NgbDate, NgbCalendar, NgbDateParserFormatter } from '@ng-bootstrap/ng-b
 })
 export class AppComponent {
   title = 'travel';
-  hoveredDate: NgbDate | null = null;
-  fromDate: NgbDate | null;
-  toDate: NgbDate | null;
   country: boolean;
   hotel: boolean;
   ticket: boolean;
   visibleTextAboveUs: boolean;
-
-  constructor(private calendar: NgbCalendar, public formatter: NgbDateParserFormatter) {
-    this.fromDate =null;
-    this.toDate = null;
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl()
+  });
+  
+  constructor() {
     this.visibleTextAboveUs=true;
     this.country=false;
     this.hotel=false;
     this.ticket=false;
-  }
-
-  onDateSelection(date: NgbDate) {
-    if (!this.fromDate && !this.toDate) {
-      this.fromDate = date;
-    } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
-      this.toDate = date;
-    } else {
-      this.toDate = null;
-      this.fromDate = date;
-    }
-  }
-
-  isHovered(date: NgbDate) {
-    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
-  }
-
-  isInside(date: NgbDate) {
-    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-  }
-
-  isRange(date: NgbDate) {
-    return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
-  }
-
-  validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
-    const parsed = this.formatter.parse(input);
-    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
   }
   visibleCountry(): void {
     this.visibleTextAboveUs=false;
--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule }   from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { CountryComponent } from '..//modules/country/country.component';
 import { HotelComponent } from '..//modules/hotel/hotel.component';
 import { TicketComponent } from '..//modules/ticket/ticket.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatNativeDateModule} from '@angular/material/core';
 
 
 @NgModule({
@@ -20,10 +23,14 @@ import { TicketComponent } from '..//modules/ticket/ticket.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
-    NgbDatepickerModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
